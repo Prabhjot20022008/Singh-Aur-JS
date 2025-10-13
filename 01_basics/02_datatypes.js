291cc125a10581badc44e68859de20ef32141186
Prabhjot Singh <prabhjotsi2008@gmail.com>
2025-10-13 03:25:06 +0000

    datatypes DONE

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert(3+3); // we are using nodejs, not browser
+
+console.log(3
+    +3); // code readability should be high (not like this one)
+console.log("Prabhjot Singh")
+
+let name = "Prabh" // string
+let age = 17 // number
+let isLoggedIn = true // boolean 
+let state = null
+
+        // PRIMITIVE DATATYPES
+// number => 2^53 range
+// BigInt => for larger numbers
+// String => use "" or ''
+// boolean => true/false
+// null => stand-alone value // represent empty value (it is not 0)
+// undefined => when value of variable is not defined
+// symbol => used for uniqueness
+
+// object
+
+console.log(typeof null); // object 
+console.log(typeof undefined); // undefined
