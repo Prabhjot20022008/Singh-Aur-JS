3f7db035d7640ce685b4f572341cf15e1ac3125b
Prabhjot Singh <prabhjotsingh20022008@gmail.com>
2025-10-14 21:13:38 +0530

    comparison DONE

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+// **************** BASIC-COMPARISON *******************
+
+// console.log(2>1); // true
+// console.log(2>=1); // true
+// console.log(2 < 1); // false
+// console.log(2 == 1); // false
+// console.log(2 != 1); // true
+
+// **************** COMPLEX PART ******************
+// (try to avoid such complex unnecessary conversion in real-world)
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // false
+
+console.log(undefined==0); // false
+console.log(undefined == 0); // false
+console.log(undefined >= 0); // false
+console.log(undefined <= 0); // false
+
+
+// STRICT CHECK (checking datatypes and values both)
+
+console.log("2"==2); // true
+console.log("2"===2); // false
