f9199a8f217bb46e5957382a350bdcce78bad6dc
Prabhjot Singh <prabhjotsingh20022008@gmail.com>
2025-10-15 16:55:07 +0530

    minor tweaks in null comparisons

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -11,9 +11,9 @@ console.log(2 != 1); // true
 console.log("2">1);
 console.log("02">1);
 
-console.log(null > 0); // false
-console.log(null == 0); // false
-console.log(null >= 0); // false
+console.log(null > 0); // false  (> treats null as 0)
+console.log(null == 0); // false (== treats null as NaN) 
+console.log(null >= 0); // true  (>= treats null as 0) 
 
 console.log(undefined==0); // false
 console.log(undefined == 0); // false
